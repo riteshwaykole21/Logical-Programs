6608aca5f048cccce3dd27e87cfd08086fd76150
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-19 16:05:15 +0530

    [Ritesh]add :Solving Fibonacci Series Problem

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacciseries.DisplayFibonacciSeries(); 
+            Console.ReadLine();
+        }
+    }
+}
